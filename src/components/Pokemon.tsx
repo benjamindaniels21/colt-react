3e0f034f75506ad5445b54deccf219f0c61d0da4
Ben Daniels <benjamindaniels21@gmail.com>
2024-01-15 13:18:23 -0500

    styling each poke

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,9 +1,10 @@
+import "./pokemon.css"
 
 const Pokemon = () => {
   const randomNum = Math.floor(Math.random() * 150) + 1;
   
   return (
-    <div>
+    <div className="pokemon">
       <h1>Pokemon #{randomNum}</h1>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNum}.png`} alt="Random Pokemon" />
 
